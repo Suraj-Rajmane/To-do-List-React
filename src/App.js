421cfcbaa269973f9609d93b41568eadad6f94fd
suraj009 <surajrajmane18@gmail.com>
2021-03-03 14:49:57 +0530

    app.js moved to src folder from components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/App.css";
-import TodoContainer from "./TodoContainer";
+import TodoContainer from "./components/TodoContainer";
 
 function App() {
 	return (
